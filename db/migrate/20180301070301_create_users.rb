a6222aabbcade2337d931921e2c9ab9c83a369a2
Hao <hao19970819@gmail.com>
2018-02-28 23:44:44 -0800

    Make user model, secure passwords, sign up validation, and lots of tests

--- db/migrate/20180301070301_create_users.rb
+++ db/migrate/20180301070301_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214215533) do
+ActiveRecord::Schema.define(version: 20180301074115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,10 +32,12 @@ ActiveRecord::Schema.define(version: 20180214215533) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "articles"
