35aa86eeb03de85d8d371baff93c2dbcafae2fd3
lolitamm <36055969+lolitamm@users.noreply.github.com>
2018-02-28 18:18:19 -0800

    Update comment_test.rb
    if there is an error, delete this part:
    test "can save comment without title" do
          comment = Comment.new
          assert_not comment.save, "Saved the comment without a commenter"
        end
       
       test "should report error" do
          # some_undefined_variable is not defined elsewhere in the test case
          assert_raises(NameError) do
            some_undefined_variable
          end
        end

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -7,4 +7,11 @@ class CommentTest < ActiveSupport::TestCase
       assert_not comment.save, "Saved the comment without a commenter"
     end
    
+   test "should report error" do
+      # some_undefined_variable is not defined elsewhere in the test case
+      assert_raises(NameError) do
+        some_undefined_variable
+      end
+    end
+   
 end
