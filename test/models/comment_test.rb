68a0c4bb83767334fec2a2dae9250b80a25ebb67
lolitamm <36055969+lolitamm@users.noreply.github.com>
2018-02-28 17:24:33 -0800

    Update comment_test.rb
    if there is an error, remove this part:
    test "can save comment without title" do
          comment = Comment.new
          assert_not comment.save, "Saved the comment without a commenter"
        end

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
    
+   test "can save comment without title" do
+      comment = Comment.new
+      assert_not comment.save, "Saved the comment without a commenter"
+    end
+   
 end
