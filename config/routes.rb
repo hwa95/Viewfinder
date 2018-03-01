876426ec5d071d9d98dfa6a4f037c617e66f53fa
Hao <hao19970819@gmail.com>
2018-02-28 16:48:00 -0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   end
 
   #root 'welcome#index'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
