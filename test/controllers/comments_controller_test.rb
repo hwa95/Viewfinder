3a99d18bb88b05a73a9912c4ed771915f50bb578
lolitamm <36055969+lolitamm@users.noreply.github.com>
2018-02-28 17:16:41 -0800

    Update comment_controller_test.rb
    if there is a error in this file, delete this part
      test "can save comment without title" do
        comment = Comment.new
        assert_not comment.save, "Saved the comment without a commenter"
      end

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,4 +4,9 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "can save comment without title" do
+    comment = Comment.new
+    assert_not comment.save, "Saved the comment without a commenter"
+  end
+  
 end
