771cdf94341ffe873986c256c3e1b458e39558a8
Hao <hao19970819@gmail.com>
2018-02-28 16:27:19 -0800

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :users
 
   get 'welcome/index'
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
-
+  #root 'welcome#index'
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
