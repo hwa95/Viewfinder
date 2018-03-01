1fed6e36308e45751f937565bc8aa72e74a181c5
lolitamm <36055969+lolitamm@users.noreply.github.com>
2018-02-28 18:19:48 -0800

    Update comments_controller_tests.rb
    if there is an error, delete this part:
    test "should get comment home page" do
        get articles_path(@article)
        assert_response :success
      end

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,9 +4,9 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "can save comment without title" do
-    comment = Comment.new
-    assert_not comment.save, "Saved the comment without a commenter"
-  end
+ test "should get comment home page" do
+    get articles_path(@article)
+    assert_response :success
+  end 
   
 end
