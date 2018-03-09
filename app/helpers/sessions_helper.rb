14329e43cf40e9b1d7cc395e9ab2e9439db3006a
Hao <hao19970819@gmail.com>
2018-03-08 20:32:21 -0800

    remove the useless button on the header, create some tests for advanced login and let them pass, finish the advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,10 +32,6 @@ module SessionsHelper
     end
   end
 
-  # 返回当前登录的用户（如果有的话）
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 
   # 如果用户已登录，返回 true，否则返回 false
   def logged_in?
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,39 +5,53 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   def setup
-      @user = users(:michael)
-    end
+    @user = users(:michael)
+  end
 
   test "login with invalid information" do
-      get login_path
-      assert_template 'sessions/new'
-      post login_path, params: { session: { email: "", password: "" } }
-      assert_template 'sessions/new'
-      assert_not flash.empty?
-      get root_path
-      assert flash.empty?
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+      password: 'password' } }
+      assert is_logged_in?
+      assert_redirected_to @user
+      follow_redirect!
+      assert_template 'users/show'
+      assert_select "a[href=?]", login_path, count: 0
+      assert_select "a[href=?]", logout_path
+      assert_select "a[href=?]", user_path(@user)
+      delete logout_path
+      assert_not is_logged_in?
+      assert_redirected_to root_url
+      # 模拟用户在另一个窗口中点击退出链接
+      delete logout_path
+      follow_redirect!
+      assert_select "a[href=?]", login_path
+      assert_select "a[href=?]", logout_path,      count: 0
+      assert_select "a[href=?]", user_path(@user), count: 0
     end
 
-    test "login with valid information followed by logout" do
-        get login_path
-        post login_path, params: { session: { email:    @user.email,
-                                              password: 'password' } }
-        assert is_logged_in?
-        assert_redirected_to @user
-        follow_redirect!
-        assert_template 'users/show'
-        assert_select "a[href=?]", login_path, count: 0
-        assert_select "a[href=?]", logout_path
-        assert_select "a[href=?]", user_path(@user)
-        delete logout_path
-        assert_not is_logged_in?
-        assert_redirected_to root_url
-        # 模拟用户在另一个窗口中点击退出链接
-        delete logout_path
-        follow_redirect!
-        assert_select "a[href=?]", login_path
-        assert_select "a[href=?]", logout_path,      count: 0
-        assert_select "a[href=?]", user_path(@user), count: 0
-      end
+    test "login with remembering" do
+      log_in_as(@user, remember_me: '1')
+      assert_not_empty cookies['remember_token']
+    end
 
-end
+    test "login without remembering" do
+      # 登录，设定 cookie
+      log_in_as(@user, remember_me: '1')
+      delete logout_path
+      # 再次登录，确认 cookie 被删除了
+      log_in_as(@user, remember_me: '0')
+      assert_empty cookies['remember_token']
+    end
+
+  end
