c80c1450c1b61464be69c39ec11ba3ca5683b3c7
Hao <hao19970819@gmail.com>
2018-03-08 18:37:14 -0800

    add some tests for authenticated and make the tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
   # 如果指定的令牌和摘要匹配，返回 true
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,7 +7,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
       @user = users(:michael)
     end
-  
+
   test "login with invalid information" do
       get login_path
       assert_template 'sessions/new'
@@ -17,7 +17,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
       get root_path
       assert flash.empty?
     end
-    
+
     test "login with valid information followed by logout" do
         get login_path
         post login_path, params: { session: { email:    @user.email,
@@ -32,10 +32,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         delete logout_path
         assert_not is_logged_in?
         assert_redirected_to root_url
+        # 模拟用户在另一个窗口中点击退出链接
+        delete logout_path
         follow_redirect!
         assert_select "a[href=?]", login_path
         assert_select "a[href=?]", logout_path,      count: 0
         assert_select "a[href=?]", user_path(@user), count: 0
       end
-    
+
 end
